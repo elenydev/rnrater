abb99ae63cc0105f7529ba7b600d70c5f378da0c
damian <godevdamian@gmail.com>
2021-12-26 22:44:41 +0100

    Init of webhooks

--- components/Auth/SignUp/SignUp.tsx
+++ components/Auth/SignUp/SignUp.tsx
@@ -14,6 +14,7 @@ import { getFormManager } from "../../../managers/FormManager/selectors";
 import { FormInstanceName } from "../../../managers/FormManager/enums";
 import { createUserTrigger } from "../domain/actions";
 
+
 export const SignUp = () => {
   const dispatch = useDispatch();
   const [imagePreview, setImagePreview] = useState<string | undefined>();
---
+++ services/sockets/index.ts
@@ -0,0 +1,4 @@
+import { io } from "socket.io-client";
+import { DATABASE_URL } from "../../utils/api";
+
+export const socket = io(DATABASE_URL);
