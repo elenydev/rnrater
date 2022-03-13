9a7392535a56f3b5c3c5d060f708853c7f7ee72d
damian <godevdamian@gmail.com>
2022-03-13 17:38:26 +0100

    Creating button for adding categories route

--- components/Categories/Categories/Categories.tsx
+++ components/Categories/Categories/Categories.tsx
@@ -1,11 +1,13 @@
 import { Category } from "../../../infrastructure/components/interfaces/Category";
 import * as React from "react";
 
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import CategoryCard from "../../Categories/Categories/CategoryCard/CategoryCard";
 import Loader from "../../../components/Loader";
 import { useCategories } from "../hooks/useCategories";
+import { View } from "../../../components/Themed";
+import { Button } from "react-native-elements";
 
 const dummyCat: Category[] = [
   {
@@ -36,10 +38,30 @@ export default function Categories() {
   return isLoading ? (
     <Loader />
   ) : (
-    <FlatList
-      data={dummyCat}
-      keyExtractor={(item) => item.name}
-      renderItem={(itemData) => <CategoryCard category={itemData.item} />}
-    />
+    <>
+      <FlatList
+        data={dummyCat}
+        keyExtractor={(item) => item.name}
+        renderItem={(itemData) => <CategoryCard category={itemData.item} />}
+        ListFooterComponent={
+          <View style={styles.buttonBox}>
+            <Button title={"Add Category"} style={styles.button} />
+          </View>
+        }
+      />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonBox: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingVertical: 20,
+  },
+  button: {
+    width: "30%",
+  },
+});
