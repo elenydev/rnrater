bd241c35f4b775a1c18a940a43893d419f2df9e4
damian <godevdamian@gmail.com>
2022-04-10 11:59:03 +0200

    Displayng category image on list

--- components/Categories/domain/interfaces.ts
+++ components/Categories/domain/interfaces.ts
@@ -1,10 +1,10 @@
-import { Category } from "../../../infrastructure/models/Category";
+import { Category, CategoryWithCover } from "../../../infrastructure/models/Category";
 import { Paging } from "../../../infrastructure/api/interfaces";
 
 export interface CategoriesStore {
   isLoading: boolean;
   paging: Paging;
-  list: Category[];
+  list: CategoryWithCover[];
 }
 
 export interface GetCategoriesCoverImagesParams {
