72e08e9d3f2733cf5394a42af6e6ecf5add09e2a
damian <godevdamian@gmail.com>
2022-05-15 11:53:17 +0200

    Setting route title on category item

--- components/Categories/Categories/CategoryCard/CategoryEntitiesList/CategoryEntity/CategoryEntityItem/CategoryEntityItem.tsx
+++ components/Categories/Categories/CategoryCard/CategoryEntitiesList/CategoryEntity/CategoryEntityItem/CategoryEntityItem.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Text } from "../../../../../../../components/Themed";
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { CategoryStackRoutesProps } from "../../../../../../../infrastructure/router/interfaces";
+import { CategoryStackRoutes } from "../../../../../../../infrastructure/router/enums";
 
 const CategoryEntityItem = () => {
-  return (
-    <div>CategoryEntityItem</div>
-  )
-}
+  const { params } =
+    useRoute<
+      CategoryStackRoutesProps<CategoryStackRoutes.CategoryEntityItem>
+    >();
+  const navigation = useNavigation();
 
-export default CategoryEntityItem
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ title: params.categoryEntityTitle });
+  }, [params.categoryEntityTitle]);
+
+  return <Text></Text>;
+};
+
+export default CategoryEntityItem;
