4a942746352e6a76ebfe476a8b3ce5c37f09a91f
damian <godevdamian@gmail.com>
2022-06-11 12:35:02 +0200

    Added call in apis

---
+++ api/categoryPost/post/index.ts
@@ -0,0 +1,18 @@
+import { API_URL } from "../../../utils/api";
+import { post } from "../../../factories/Post";
+import { BaseRequestResponse } from "../../../infrastructure/api/interfaces";
+import { PostCategoryPostParams } from "./interfaces";
+
+export const createCategoryPost = async (
+  data: PostCategoryPostParams
+): Promise<BaseRequestResponse> => {
+  const formData = new FormData();
+  formData.append("title", data.title);
+  formData.append('categoryId', data.categoryId)
+  const categoryImage = {
+    ...data.categoryPostImage,
+    type: `image/${data.categoryPostImage.type}`,
+  };
+  formData.append("categoryPostImage", categoryImage);
+  return await post(API_URL.CATEGORY_POST.CREATE_CATEGORY_POST, formData, true, true);
+};
